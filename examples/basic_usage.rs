7c4782665811e995e29002b5a9c4def8221be0a7
orhanbalci@gmail.com <orhanbalci@gmail.com>
2019-07-16 22:34:02 +0300

    middleware architecture updated to match actix-web 1.0

--- examples/basic_usage.rs
+++ examples/basic_usage.rs
@@ -2,22 +2,22 @@ extern crate actix;
 extern crate actix_prometheus;
 extern crate actix_web;
 
-use actix_prometheus::PrometheusMiddleware;
-use actix_web::{server, App};
+use actix_prometheus::register_default_counters;
+use actix_prometheus::PrometheusTransform;
+use actix_web::{http, web, HttpResponse};
+use actix_web::{App, HttpServer};
 
 fn main() {
     println!("Running prometheus example");
-    let sys = actix::System::new("prometheus-example");
 
-    let _addr = server::new(|| {
-        App::new()
-            .middleware(PrometheusMiddleware::default())
-            .resource("/", |r| {
-                r.f(|_| "Hello, middleware! Check the console where the server is run.")
-            })
-    }).bind("127.0.0.1:8080")
-        .unwrap()
-        .start();
-
-    let _ = sys.run();
+    register_default_counters();
+    HttpServer::new(|| {
+        App::new().wrap(PrometheusTransform).service(
+            web::resource("/test").route(web::get().to(|| HttpResponse::Ok().body("Well done!"))),
+        )
+    })
+    .bind("127.0.0.1:8088")
+    .unwrap()
+    .run()
+    .unwrap()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,51 +4,106 @@ extern crate prometheus;
 #[macro_use]
 extern crate lazy_static;
 
-use actix_web::middleware::{Finished, Middleware, Response, Started};
-use actix_web::{HttpRequest, HttpResponse, Result};
+extern crate futures;
 
-use prometheus::{Counter, IntCounter};
+use actix_web::{dev::Service, dev::Transform};
+use actix_web::{dev::ServiceRequest, dev::ServiceResponse, Error};
+use futures::future::{ok, FutureResult};
+use futures::{Future, Poll};
+
+use prometheus::{Counter, IntCounter, IntCounterVec};
 
 use std::collections::HashMap;
 use std::sync::Mutex;
 
-lazy_static!{
-static ref Collectors : Mutex<HashMap<&'static str, Box<IntCounter>>> = {
+lazy_static! {
+    static ref INT_COUNTERS: Mutex<HashMap<&'static str, Box<IntCounter>>> = {
+        let m = HashMap::new();
+        Mutex::from(m)
+    };
+    static ref INT_VEC_COUNTERS: Mutex<HashMap<&'static str, Box<IntCounterVec>>> = {
         let m = HashMap::new();
         Mutex::from(m)
     };
 }
 
-pub struct PrometheusMiddleware;
+pub fn register_default_counters() {
+    let opts = opts!(
+        "request_count",
+        "Number of HTTP requests processed. Partitioned by status code and HTTP method"
+    );
+    INT_VEC_COUNTERS.lock().unwrap().insert(
+        "request_count",
+        Box::from(register_int_counter_vec!(opts, &["status_code", "http_method"]).unwrap()),
+    );
+}
 
-impl Default for PrometheusMiddleware {
-    fn default() -> Self {
-        PrometheusMiddleware {}
+pub struct PrometheusTransform;
+
+// Middleware factory is `Transform` trait from actix-service crate
+// `S` - type of the next service
+// `B` - type of response's body
+impl<S, B> Transform<S> for PrometheusTransform
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = PrometheusMiddleware<S>;
+    type Future = FutureResult<Self::Transform, Self::InitError>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ok(PrometheusMiddleware { service })
     }
 }
 
-impl PrometheusMiddleware {
+pub struct PrometheusMiddleware<S> {
+    service: S,
+}
+
+impl<S> PrometheusMiddleware<S> {
     pub fn register_int_counter(&self) {
-        Collectors.lock().unwrap().insert(
+        INT_COUNTERS.lock().unwrap().insert(
             "hit_count",
             Box::from(register_int_counter!("hit_count", "hit count help").unwrap()),
         );
     }
 }
 
-impl<S> Middleware<S> for PrometheusMiddleware {
-    fn start(&self, req: &HttpRequest<S>) -> Result<Started> {
+impl<S, B> Service for PrometheusMiddleware<S>
+where
+    S: Service<Request = ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Request = ServiceRequest;
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        self.service.poll_ready()
+    }
+
+    fn call(&mut self, req: ServiceRequest) -> Self::Future {
         println!("Prometheus middleware started");
-        Ok(Started::Done)
-    }
 
-    fn response(&self, req: &HttpRequest<S>, resp: HttpResponse) -> Result<Response> {
-        println!("Prometheus middleware response");
-        Ok(Response::Done(resp))
-    }
-
-    fn finish(&self, req: &HttpRequest<S>, resp: &HttpResponse) -> Finished {
-        println!("Prometheus middleware finished");
-        Finished::Done
+        Box::new(self.service.call(req).and_then(|res| {
+            println!("Prometheus middleware response");
+            INT_VEC_COUNTERS
+                .lock()
+                .unwrap()
+                .get("request_count")
+                .unwrap()
+                .get_metric_with_label_values(&[
+                    res.status().as_str(),
+                    res.request().method().as_str(),
+                ]);
+            Ok(res)
+        }))
     }
 }
